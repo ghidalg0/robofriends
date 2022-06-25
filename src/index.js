79bf3c5897aeb97c77ada10f7fcc82719ec07171
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-06-25 16:43:34 +0200

    seting up files

--- src/index.js
+++ src/index.js
@@ -7,11 +7,16 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <h1>Hello World</h1>
     <App />
   </React.StrictMode>
 );
+reportWebVitals();
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
