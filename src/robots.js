0e4a5e67a4b4ae5b5a13cd7c1d27ed75f5e53dfb
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-06-25 17:37:17 +0200

    ✅ first dynamic robo cards created

---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'King Bob',
+    username: 'Bob',
+    email: 'bob@bob.com'
+  },
+  {
+    id: 2,
+    name: 'Loo Stuart',
+    username: 'Stu',
+    email: 'stu@bob.com'
+  },
+  {
+    id: 3,
+    name: 'Boulou lou',
+    username: 'Boulou',
+    email: 'boulou@bob.com'
+  },
+  {
+    id: 4,
+    name: 'Moooooh',
+    username: 'Mo',
+    email: 'mo@bob.com'
+  },
+  {
+    id: 5,
+    name: 'Kala kala',
+    username: 'Kala',
+    email: 'kal@bob.com'
+  },
+  {
+    id: 6,
+    name: 'Rititi',
+    username: 'Ri',
+    email: 'rititi@bob.com'
+  },
+  {
+    id: 7,
+    name: 'KoKaKi',
+    username: 'koka',
+    email: 'koka@bob.com'
+  },
+]
