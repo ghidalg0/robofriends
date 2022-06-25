3a06b3709af07fed6363c9c4ddcc5fa20f4b9b6b
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-06-25 18:51:02 +0200

    ✅ css styling of the app

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -4,7 +4,7 @@ const SearchBox = ({ searchChange }) => {
   return (
     <div className='pa2'>
       <input
-        className="pa3 ba b--green bg-light-blue"
+        className="pa3 ba b--green"
         type='search'
         placeholder="Search your friend"
         onChange= {searchChange}
