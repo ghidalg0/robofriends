877216a2bb2c4c8a186713b3b48f4f8ccb99be0f
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-06-26 17:56:45 +0200

    Updates

---
+++ precache-manifest.621c4e25048d10fdbaa65d3feace61ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cf59a145d4cfce42ab0",
+    "url": "/robofriends/static/css/main.8165f35f.chunk.css"
+  },
+  {
+    "revision": "2cf59a145d4cfce42ab0",
+    "url": "/robofriends/static/js/main.86d04d0a.chunk.js"
+  },
+  {
+    "revision": "75dcfc39fa8879b648fd",
+    "url": "/robofriends/static/js/runtime~main.e6791cfe.js"
+  },
+  {
+    "revision": "b8a9d7eb35f95ecec54f",
+    "url": "/robofriends/static/css/2.0e381eae.chunk.css"
+  },
+  {
+    "revision": "b8a9d7eb35f95ecec54f",
+    "url": "/robofriends/static/js/2.78e1aefb.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/robofriends/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "e29bdd72bda72b015c1236295bce491e",
+    "url": "/robofriends/index.html"
+  }
+];
